8ba6ae7aac2f8b11b0ca9daac72cf708ffaae86f
neraj.upreti <neraj.upreti@np.technofex.com>
2024-03-20 22:12:30 +0545

    intial snapshot

---
+++ VoiceWizard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VoiceWizard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
